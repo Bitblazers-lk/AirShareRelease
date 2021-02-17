179601e4252170c627c91adf22d4f31e0290aadf
HasinduLanka <hlrathnayaka@gmail.com>
2021-02-17 11:09:17 +0530

    cp fix

--- install-linux.sh
+++ install-linux.sh
@@ -3,19 +3,19 @@
 
 rm -f -d -r /usr/share/AirShare/AirShareRelease
 
-mkdir -p /usr/share/AirShare/
-cd /usr/share/AirShare
-git clone --single-branch --branch linux64sc --depth 1 https://github.com/Bitblazers-lk/AirShareRelease 
-
-sudo chmod +x /usr/share/AirShare/AirShareRelease/AirShare
-
-
 sudo mkdir -p /usr/local/bin/
 sudo cp -f -r ./shell-installed/* /usr/local/bin/
 
 sudo chmod +x /usr/local/bin/airshare
 sudo chmod +x /usr/local/bin/airshare-update
 sudo chmod +x /usr/local/bin/airshare-autostart
+
+
+mkdir -p /usr/share/AirShare/
+cd /usr/share/AirShare
+git clone --single-branch --branch linux64sc --depth 1 https://github.com/Bitblazers-lk/AirShareRelease 
+
+sudo chmod +x /usr/share/AirShare/AirShareRelease/AirShare
 
 
 echo "Completed"
