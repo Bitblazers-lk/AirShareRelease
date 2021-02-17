ae954b52e9de57367922b1a8b1d22d92cf1fccef
HasinduLanka <hlrathnayaka@gmail.com>
2021-02-17 11:08:04 +0530

    fix

--- install-linux.sh
+++ install-linux.sh
@@ -3,13 +3,6 @@
 
 rm -f -d -r /usr/share/AirShare/AirShareRelease
 
-mkdir -p /usr/share/AirShare/
-cd /usr/share/AirShare
-git clone --single-branch --branch linux64fd --depth 1 https://github.com/Bitblazers-lk/AirShareRelease 
-
-sudo chmod +x /usr/share/AirShare/AirShareRelease/AirShare
-
-
 sudo mkdir -p /usr/local/bin/
 sudo cp -f -r ./shell-installed/* /usr/local/bin/
 
@@ -17,5 +10,11 @@ sudo chmod +x /usr/local/bin/airshare
 sudo chmod +x /usr/local/bin/airshare-update
 sudo chmod +x /usr/local/bin/airshare-autostart
 
+
+mkdir -p /usr/share/AirShare/
+cd /usr/share/AirShare
+git clone --single-branch --branch linux64fd --depth 1 https://github.com/Bitblazers-lk/AirShareRelease 
+
+sudo chmod +x /usr/share/AirShare/AirShareRelease/AirShare
 
 echo "Completed"
