32a9a532de4e98169b674d0d486122fada0a31b5
HasinduLanka <hlrathnayaka@gmail.com>
2021-02-17 08:03:49 +0530

    installer

--- install-linux.sh
+++ install-linux.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-sudo mkdir -p /usr/share/AirShare/AirShareRelease/
-sudo mkdir -p /usr/local/bin/
 
-sudo cp -r ./* /usr/share/AirShare/AirShareRelease/
+rm -f -d -r /usr/share/AirShare/AirShareRelease
+
+mkdir -p /usr/share/AirShare/
+cd /usr/share/AirShare
+git clone --single-branch --branch linux64sc --depth 1 https://github.com/Bitblazers-lk/AirShareRelease 
+
 sudo chmod +x /usr/share/AirShare/AirShareRelease/AirShare
 
-sudo cp -r ./shell-installed/* /usr/local/bin/
+
+sudo mkdir -p /usr/local/bin/
+sudo cp -f -r ./shell-installed/* /usr/local/bin/
 
 sudo chmod +x /usr/local/bin/airshare
 sudo chmod +x /usr/local/bin/airshare-update
 sudo chmod +x /usr/local/bin/airshare-autostart
+
 
 echo "Completed"
